d5d6eeeddfd1d2eaecfe516c98ae39c84497e2e3
EnjiAlzamzami <99639925+EnjiAlzamzami@users.noreply.github.com>
2022-12-05 11:03:48 +0300

    Improving a comments, likes counter

--- app/src/main/java/com/example/socialmediaprojectkk/PostsAdapter.kt
+++ app/src/main/java/com/example/socialmediaprojectkk/PostsAdapter.kt
@@ -2,6 +2,7 @@ package com.example.socialmediaproject
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
@@ -28,6 +29,8 @@ class PostsAdapter(private var context : Context) :
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         var pk = 0
+        var likesCounter=0
+        var commentsCounter=0
 
         holder.binding.apply {
             nameTv.text=getItem(position).title
@@ -35,9 +38,24 @@ class PostsAdapter(private var context : Context) :
             var likeList = getItem(position).likes.split(",")
             //Split comments string and store it into list
             var commentList = getItem(position).comments.split(",")
-
-            commentTv.text = "Comments ${commentList.size}"
-            likesTv.text = "Likes ${likeList.size}-"
+            //For loop to calculate how many likes the user got
+            for(i in likeList){
+                if(i!=""){
+                    likesCounter++
+                }else{
+                    Log.d("MainActivity", "likeList: $likeList")
+                }
+            }
+            //For loop to calculate how many comments the user got
+            for (j in commentList){
+                if(j!=""){
+                    commentsCounter++
+                }else{
+                    Log.d("MainActivity", "likeList: $commentList")
+                }
+            }
+            commentTv.text = "$commentsCounter Comments  "
+            likesTv.text = "$likesCounter Likes"
             pk = getItem(position).id
             namePlace.setOnClickListener {
                 var postDetailsIntent= Intent(context,DetailsActivity::class.java)
